9fbfbbc0e819faf89750549e7a60c7b153a88b83
tri le <letrin93@gmail.com>
2022-12-04 13:24:33 -0700

    tri

---
+++ pages/admin/[slug].js
@@ -0,0 +1,8 @@
+import React from "react"
+export default function AdminPostEdit({ }) {
+  return (
+    <main>
+      <h1>Edit Post</h1>
+    </main>
+  )
+}
---
+++ pages/enter.js
@@ -0,0 +1,8 @@
+import React from "react"
+export default function EnterPage({ }) {
+  return (
+    <main>
+      h1
+    </main>
+  )
+}
